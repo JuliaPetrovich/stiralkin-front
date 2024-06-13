b199e666080c2e9860ab81d8b8f558e6ba3d830f
zimirken1 <rumyan1337@gmail.com>
2024-06-13 20:32:58 +0300

    fix: add error alert

--- js/form.js
+++ js/form.js
@@ -23,45 +23,52 @@ document.getElementById('contactForm').addEventListener('submit', function (even
 
     const jsonData = JSON.stringify(object);
 
-    fetch('https://mailsender-production-5fa1.up.railway.app/api/v1/mail', {
+    fetch('http://localhost:9090/api/v1/mail', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: jsonData
     })
-        .then(response => response.text())
-        .then(text => {
-            this.reset();
-            Swal.fire({
-                icon: "success",
-                title: "Ваша заявка успешно отправлена!",
-                text: "С Вами свяжутся в ближайшее время",
-                customClass: {
-                    title: 'my-title-style',
-                    content: 'my-content-style',
-                },
-                showCloseButton: true,
-                showConfirmButton: false
+        .then(response => {
+        if (!response.ok) {
+            return response.json().then(err => {
+                throw new Error(err.message);
             });
-        })
-        .catch((error) => {
-            console.error('There was a problem with your fetch operation:', error);
-            Swal.fire({
-                icon: 'error',
-                title: 'Упс...',
-                text: 'Что-то пошло не так!',
-                customClass: {
-                    title: 'my-title-style',
-                    content: 'my-content-style',
-                },
-                showCloseButton: true,
-                showConfirmButton: false
-            });
-        })
-        .finally(() => {
-            submitButton.disabled = false;
-            loader.style.display = 'none';
-            buttonText.style.display = 'inline-block';
+        }
+        return response.text();
+    })
+    .then(text => {
+        this.reset();
+        Swal.fire({
+            icon: "success",
+            title: "Ваша заявка успешно отправлена!",
+            text: "С Вами свяжутся в ближайшее время",
+            customClass: {
+                title: 'my-title-style',
+                content: 'my-content-style',
+            },
+            showCloseButton: true,
+            showConfirmButton: false
         });
+    })
+    .catch((error) => {
+        console.error('There was a problem with your fetch operation:', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Упс...',
+            text: 'Что-то пошло не так, повторите попытку позже',
+            customClass: {
+                title: 'my-title-style',
+                content: 'my-content-style',
+            },
+            showCloseButton: true,
+            showConfirmButton: false
+        });
+    })
+    .finally(() => {
+        submitButton.disabled = false;
+        loader.style.display = 'none';
+        buttonText.style.display = 'inline-block';
+    })
 });
